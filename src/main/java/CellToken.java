056218017c8ca830265bfb9bd4f57b4a6661b7c0
Tinytin06 <52684983+Tinytin06@users.noreply.github.com>
2022-02-26 20:45:31 -0800

    testing if this works
    testing if this works

---
+++ src/main/java/CellToken.java
@@ -0,0 +1,7 @@
+public class CellToken {
+    public int getRow() {
+    }
+
+    public int getColumn() {
+    }
+}
---
+++ src/main/java/Spreadsheet.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+public class Spreadsheet {
+    public void printValues() {
+    }
+
+    public int getNumRows() {
+    }
+
+    public int getNumColumns() {
+    }
+    CellToken getCellToken(String place, int i, CellToken Cell){
+
+    }
+    public void printCellFormula(CellToken cellToken) {
+    }
+
+    public void printAllFormulas() {
+    }
+
+    public void changeCellFormulaAndRecalculate(CellToken cellToken, Stack expTreeTokenStack) {
+    }
+}
