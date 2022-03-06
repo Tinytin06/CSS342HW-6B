1b492ee8c5307d7796c0a0547703318ac13e84ff
alexzrosario <alexzrosario@gmail.com>
2022-03-06 14:01:05 -0800

    Added - recalculate the values that are already in the array

--- Cloneable/Spreadsheet.java
+++ Cloneable/Spreadsheet.java
@@ -5,9 +5,6 @@ public class Spreadsheet {
 	private static int ROW;
 	private static int COLUMN;
 	private Cell[][] spreadsheetArray;
-	//private Graph graph = new Graph();
-	//maybe need a counter
-	private static int count;
 	/**
 	 * Default Constructor - sets row and column to 5
 	 */
@@ -77,6 +74,9 @@ public class Spreadsheet {
 	 * Prints all formulas of spreadsheet into the GUI
 	 */
 	public void printValues() {
+		//recalc all values
+		recalcAll();
+
 		Object[][] data = new Object[ROW][COLUMN];
 		for (int i = 0; i < getNumRows(); i++) {
 			for (int j = 0; j < getNumColumns(); j++) {
@@ -270,5 +270,224 @@ public class Spreadsheet {
 
 	public Cell[][] getSpreadsheetArray() {
 		return this.spreadsheetArray;
+	}
+
+	public void recalcAll(){
+		for (int i = 0; i < getNumRows(); i++) {
+			for (int j = 0; j < getNumColumns(); j++) {
+				if(this.spreadsheetArray[i][j].getFormula() != null) {
+					CellToken cellToken = new CellToken();
+					cellToken.setColumn(j);
+					cellToken.setRow(i+1);
+					String inputFormula = this.spreadsheetArray[i][j].getFormula();
+					Stack expTreeTokenStack;
+					expTreeTokenStack = getFormula(inputFormula);
+					try {
+						this.changeCellFormulaAndRecalculate(cellToken,expTreeTokenStack);
+					} catch (CloneNotSupportedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+
+	}
+
+	/**
+	 * getFormula
+	 *
+	 * Given a string that represents a formula that is an infix
+	 * expression, return a stack of Tokens so that the expression,
+	 * when read from the bottom of the stack to the top of the stack,
+	 * is a postfix expression.
+	 *
+	 * A formula is defined as a sequence of tokens that represents
+	 * a legal infix expression.
+	 *
+	 * A token can consist of a numeric literal, a cell reference, or an
+	 * operator (+, -, *, /).
+	 *
+	 * Multiplication (*) and division (/) have higher precedence than
+	 * addition (+) and subtraction (-).  Among operations within the same
+	 * level of precedence, grouping is from left to right.
+	 *
+	 * This algorithm follows the algorithm described in Weiss, pages 105-108.
+	 */
+	private Stack getFormula(String formula) {
+		Stack returnStack = new Stack();  // stack of Tokens (representing a postfix expression)
+		boolean error = false;
+		char ch = ' ';
+
+		int literalValue = 0;
+
+		CellToken cellToken;
+		int column = 0;
+		int row = 0;
+
+		int index = 0;  // index into formula
+		Stack operatorStack = new Stack();  // stack of operators
+
+		while (index < formula.length() ) {
+			// get rid of leading whitespace characters
+			while (index < formula.length() ) {
+				ch = formula.charAt(index);
+				if (!Character.isWhitespace(ch)) {
+					break;
+				}
+				index++;
+			}
+
+			if (index == formula.length() ) {
+				error = true;
+				break;
+			}
+
+			// ASSERT: ch now contains the first character of the next token.
+			if (isOperator(ch)) {
+				// We found an operator token
+				switch (ch) {
+					case OperatorToken.Plus:
+					case OperatorToken.Minus:
+					case OperatorToken.Mult:
+					case OperatorToken.Div:
+					case OperatorToken.LeftParen:
+						// push operatorTokens onto the output stack until
+						// we reach an operator on the operator stack that has
+						// lower priority than the current one.
+						OperatorToken stackOperator;
+						while (!operatorStack.isEmpty()) {
+							stackOperator = (OperatorToken) operatorStack.top();
+							if ( (stackOperator.priority() >= operatorPriority(ch)) &&
+									(stackOperator.getOperatorToken() != OperatorToken.LeftParen) ) {
+
+								// output the operator to the return stack
+								operatorStack.pop();
+								returnStack.push(stackOperator);
+							} else {
+								break;
+							}
+						}
+						break;
+
+					default:
+						// This case should NEVER happen
+						System.out.println("Error in getFormula.");
+						System.exit(0);
+						break;
+				}
+				// push the operator on the operator stack
+				operatorStack.push(new OperatorToken(ch));
+
+				index++;
+
+			} else if (ch == ')') {    // maybe define OperatorToken.RightParen ?
+				OperatorToken stackOperator;
+				stackOperator = (OperatorToken) operatorStack.topAndPop();
+				// This code does not handle operatorStack underflow.
+				while (stackOperator.getOperatorToken() != OperatorToken.LeftParen) {
+					// pop operators off the stack until a LeftParen appears and
+					// place the operators on the output stack
+					returnStack.push(stackOperator);
+					stackOperator = (OperatorToken) operatorStack.topAndPop();
+				}
+
+				index++;
+			} else if (Character.isDigit(ch)) {
+				// We found a literal token
+				literalValue = ch - '0';
+				index++;
+				while (index < formula.length()) {
+					ch = formula.charAt(index);
+					if (Character.isDigit(ch)) {
+						literalValue = (literalValue * 10) + (ch - '0');
+						index++;
+					} else {
+						break;
+					}
+				}
+				// place the literal on the output stack
+				returnStack.push(new LiteralToken(literalValue));
+
+			} else if (Character.isUpperCase(ch)) {
+				// We found a cell reference token
+				cellToken = new CellToken();
+				index = getCellToken(formula, index, cellToken);
+				if (cellToken.getRow() == BadCell) {
+					error = true;
+					break;
+				} else {
+					// place the cell reference on the output stack
+					returnStack.push(cellToken);
+				}
+
+			} else {
+				error = true;
+				break;
+			}
+		}
+
+		// pop all remaining operators off the operator stack
+		while (!operatorStack.isEmpty()) {
+			returnStack.push(operatorStack.topAndPop());
+		}
+
+		if (error) {
+			// a parse error; return the empty stack
+			returnStack.makeEmpty();
+		}
+
+		return returnStack;
+	}
+
+	/**
+	 * Given an operator, return its priority.
+	 *
+	 * priorities:
+	 *   +, - : 0
+	 *   *, / : 1
+	 *   (    : 2
+	 *
+	 * @param ch  a char
+	 * @return  the priority of the operator
+	 */
+	private static int operatorPriority (char ch) {
+		if (!isOperator(ch)) {
+			// This case should NEVER happen
+			System.out.println("Error in operatorPriority.");
+			System.exit(0);
+		}
+		switch (ch) {
+			case OperatorToken.Plus:
+				return 0;
+			case OperatorToken.Minus:
+				return 0;
+			case OperatorToken.Mult:
+				return 1;
+			case OperatorToken.Div:
+				return 1;
+			case OperatorToken.LeftParen:
+				return 2;
+
+			default:
+				// This case should NEVER happen
+				System.out.println("Error in operatorPriority.");
+				System.exit(0);
+				break;
+		}
+		return -1;
+	}
+
+	/**
+	 * Return true if the char ch is an operator of a formula.
+	 * Current operators are: +, -, *, /, (.
+	 * @param ch  a char
+	 * @return  whether ch is an operator
+	 */
+	private static boolean isOperator (char ch) {
+		return ((ch == OperatorToken.Plus) ||
+				(ch == OperatorToken.Minus) ||
+				(ch == OperatorToken.Mult) ||
+				(ch == OperatorToken.Div) ||
+				(ch == OperatorToken.LeftParen) );
 	}
 }
